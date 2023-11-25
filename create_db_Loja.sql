f83565b068ee9619629b17aa5448d90ed0c4f81f
ricardo-shinoda <ricardoshinoda@gmail.com>
2023-11-24 22:30:48 -0300

    Initializing db_develoment repo

---
+++ create_db_Loja.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE Loja;
+
+USE Loja;
+
+CREATE TABLE Clientes (
+    ClienteID INT PRIMARY KEY,
+    Nome VARCHAR(255),
+    Email VARCHAR(255)
+);
+
+CREATE TABLE Produtos (
+    ProdutoID INT PRIMARY KEY,
+    NomeProduto VARCHAR(255),
+    Preco DECIMAL(10, 2)
+);
+
+
